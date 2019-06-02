5b5db96c7e76e47781e039a02c662f74d110c899
Niko Matsakis <niko@alum.mit.edu>
2019-06-02 10:53:24 -0400

    port to tide 0.2.0

--- src/serve.rs
+++ src/serve.rs
@@ -5,13 +5,13 @@ use std::sync::Mutex;
 use uuid::Uuid;
 
 async fn serve_cards(cx: tide::Context<Mutex<MathemaRepository>>) -> tide::EndpointResult {
-    let repo = cx.state().lock().unwrap();
+    let repo = cx.app_data().lock().unwrap();
     let uuids: Vec<Uuid> = repo.cards().keys().cloned().collect();
     Ok(tide::response::json(uuids))
 }
 
 async fn serve_card(cx: tide::Context<Mutex<MathemaRepository>>) -> tide::EndpointResult {
-    let repo = cx.state().lock().unwrap();
+    let repo = cx.app_data().lock().unwrap();
     let uuid: Uuid = cx.param("uuid").unwrap();
     let card = repo.card(uuid);
     Ok(tide::response::json(card))
@@ -25,7 +25,7 @@ crate fn serve(options: &MathemaOptions) -> Fallible<()> {
             return Ok(());
         }
 
-        let mut app = tide::App::with_state(Mutex::new(repo));
+        let mut app = tide::App::new(Mutex::new(repo));
         app.at("/api/cards").get(serve_cards);
         app.at("/api/card/:uuid").get(serve_card);
         app.serve("127.0.0.1:8000")?;
